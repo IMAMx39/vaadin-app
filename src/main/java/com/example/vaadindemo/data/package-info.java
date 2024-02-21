1598d295a670940601a82c02e255a123981c1b32
start.vaadin.com <no-reply@vaadin.com>
2024-02-21 15:43:55 +0000

    Generated project

---
+++ src/main/java/com/example/vaadindemo/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.example.vaadindemo.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/example/vaadindemo/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.example.vaadindemo.services;
+
+import org.springframework.lang.NonNullApi;
